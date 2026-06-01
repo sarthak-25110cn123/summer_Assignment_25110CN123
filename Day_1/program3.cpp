a285d1c2b6e2e4391962b13537e0bd79f007fc94
sarthak-25110cn123 <25110cn123@glbitm.ac.in>
2026-06-01 22:50:18 +0530

    factorial code update

--- Day_1/program3.cpp
+++ Day_1/program3.cpp
@@ -5,9 +5,14 @@ int main() {
     int n,fact=1;
     cout<<"Enter a number: ";
     cin>>n;
+    if(n<0){
+        cout << "Factorial is not defined for negative numbers.";
+    }
+    else{
     for(int i=1;i<=n;i++){
         fact=fact*i;
     }
     cout<<"Factorial of "<<n<<" is "<<fact;
+}
     return 0;
 }
