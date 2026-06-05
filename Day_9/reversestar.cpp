085997db1b15a8a6477c99da11aea8b749ef1bb4
sarthak-25110cn123 <25110cn123@glbitm.ac.in>
2026-06-05 10:26:52 +0530

    Day_9 completed

---
+++ Day_9/reversestar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if(n<0){
+        cout<<"Enter a positive number !";
+    }
+    else{
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+    return 0;
+}
