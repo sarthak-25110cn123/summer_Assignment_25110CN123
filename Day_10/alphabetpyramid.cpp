425b7c75cc353654d54434062e2ba0394eb0a0db
sarthak-25110cn123 <25110cn123@glbitm.ac.in>
2026-06-10 09:18:59 +0530

    Day_10 submitted

---
+++ Day_10/alphabetpyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if(n<0){
+        cout<<"Enter a positive number !";
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<char(j+64);
+        }
+        
+            for(int j=i-1;j>=1;j--){
+            cout<<char(j+64);
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
